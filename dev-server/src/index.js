d849b0086e126bbd000577a6c09162a82094a60d
Christophe <christophe.sikes@outlook.com>
2025-07-01 20:30:13 +0200

    add cors and build:ui on server

--- dev-server/src/index.js
+++ dev-server/src/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
+app.use(express.static('dist'))
+app.use(cors())
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
 app.get('/', (request, response) => {
@@ -9,7 +16,7 @@ app.get('/', (request, response) => {
 })
 
  
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
