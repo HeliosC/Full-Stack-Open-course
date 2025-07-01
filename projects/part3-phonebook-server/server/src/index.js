c5d5da51bea33bf20935031d52ba73cfaca454c4
Christophe <christophe.sikes@outlook.com>
2025-07-01 20:35:08 +0200

    part3 - phonebook: fullstack and hosted - ex 3.9-3.11

--- projects/part3-phonebook-server/server/src/index.js
+++ projects/part3-phonebook-server/server/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
+app.use(express.static('dist'))
+app.use(cors())
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -85,7 +88,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
  
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
